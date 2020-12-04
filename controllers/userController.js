acc6d1c71539b97184e2c6b61e4c6365c54e4bc6
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-04 16:29:17 -0600

    Send error message as json

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,17 +8,19 @@ const {
 
 exports.register = async (req, res) => {
 	//validate user data
+	console.log('step 1');
 	const { error } = registerValidation(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
-
+	if (error) return res.status(400).json(error.details[0].message);
 	//check if username exists
+	console.log('step 2');
 	const usernameExist = await User.findOne({
 		username: req.body.username,
 	});
+	console.log('step 3');
 	if (usernameExist) {
-		return res.status(400).send('Username Already Exists');
+		return res.status(400).json('Username Already Exists');
 	}
-
+	console.log('step 4');
 	const salt = await bcrypt.genSalt(10);
 	const hashPassword = await bcrypt.hash(req.body.password, salt);
 
@@ -27,8 +29,9 @@ exports.register = async (req, res) => {
 		last_name: req.body.last_name,
 		username: req.body.username,
 		password: hashPassword,
+		status: 'viewer',
 	});
-
+	console.log('step 5');
 	//create and sign jwt token to be automatically logged in
 	const token = jwt.sign(
 		{ _id: user._id },
@@ -41,22 +44,23 @@ exports.register = async (req, res) => {
 			token: token,
 			user: {
 				username: user.username,
+				status: user.status,
 			},
 		});
 	} catch (err) {
-		res.status(400).send(err);
+		res.status(400).json(err);
 	}
 };
 
 exports.login = async (req, res) => {
 	//validate user data
 	const { error } = loginValidation(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
+	if (error) return res.status(400).json(error.details[0].message);
 
 	//check for username in db
 	const user = await User.findOne({ username: req.body.username });
 	if (!user) {
-		return res.status(400).send('Invalid Email');
+		return res.status(400).json('Invalid Email');
 	}
 
 	//check if password is correct
@@ -65,7 +69,7 @@ exports.login = async (req, res) => {
 		user.password,
 	);
 	if (!validPass) {
-		return res.status(400).send('Invalid Password');
+		return res.status(400).json('Invalid Password');
 	}
 
 	//create and sign user token
@@ -77,6 +81,7 @@ exports.login = async (req, res) => {
 		token: token,
 		user: {
 			username: user.username,
+			status: user.status,
 		},
 	});
 };
